679f776da762f37c7dcb7b44a0445f4c89e21a0e
Wasif Khan <waasif.khaan@gmail.com>
2018-08-11 23:15:33 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ def initialize(id:, name:, type:, db:)
 end 
 
 def self.save(id, name, type, db)
-  @db.execute("INSERT INTO Pokemon (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
+  @db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)",id, name, type)
 
 end 
   
