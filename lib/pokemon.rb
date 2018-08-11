dbd8cd67ef178b8e0cf0b54661dabf055e572d3f
Wasif Khan <waasif.khaan@gmail.com>
2018-08-11 22:56:36 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
-class Pokemon
-end
+class Pokemon 
+  attr_accessor :id, :name, :type, :db
+  
+  @@all = []
+
+def initialize(id:, name:, type:, db:)
+  @id = id 
+  @name = name 
+  @type = type
+  @db = db
+  
+end 
+
+def self.save(id, name, type, db)
+  #database_connection.execute("INSERT INTO #{db} (id, name,type ) VALUES (#{id},#{name},#{type})")
+  @@all << self.new
+end 
+  
+end 
+
