ddc8b84ee323eb05873dba109795810e9eaa8228
Wasif Khan <waasif.khaan@gmail.com>
2018-08-11 23:22:41 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,7 +13,7 @@ def initialize(id:, name:, type:, db:)
 end 
 
 def self.save(id, name, type)
-  self.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)",@id, @name, @type)
+  @db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)",@id, @name, @type)
 
 end 
   
