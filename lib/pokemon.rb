7a21a182fc8698f98e7dc6d174d0a8221ee90078
Wasif Khan <waasif.khaan@gmail.com>
2018-08-12 00:57:45 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,7 +16,7 @@ def self.save(name, type, db)
   db.execute("INSERT INTO pokemon(name, type) VALUES (?, ?)", name, type)
 end 
 def self.find(id,db)
-  db.execute("SELECT * FROM pokemon WHERE pokemon.id == id", id )
+  db.execute("SELECT * FROM pokemon WHERE pokemon.id == ?", id )
 end 
   
  
