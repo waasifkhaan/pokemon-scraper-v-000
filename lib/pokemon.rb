0fa59e95bda5916c4e37f99e93e543957ff435c7
Wasif Khan <waasif.khaan@gmail.com>
2018-08-12 00:19:19 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,9 +15,9 @@ end
 def self.save(name, type, db)
   db.execute("INSERT INTO pokemon(name, type) VALUES (?, ?)", name, type)
   
-def self.find(id,name)
+def self.find(name,type)
   @@all.detect do |pokemon|
-    self.new(id,name,type,db) if pokemon.id == id
+    self.new(pokemon.id,name,type,pokemon.db) if pokemon.id == id
   end 
   
 end 
